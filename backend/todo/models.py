2b572ca3711747a3829dfa50a2b413319fffb73f
Murilo Chiquetti Galina <murilochiquettigalina@gmail.com>
2026-03-04 22:25:48 -0300

    feat: Adding field to Task for external API integration.

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -16,6 +16,7 @@ class Task(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='tasks')
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_tasks')
     shared_with = models.ManyToManyField(User, blank=True, related_name='shared_tasks')
+    external_quote = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
