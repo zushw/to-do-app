606510be076b3950d672436e30f61ace95bd4ee1
Murilo Chiquetti Galina <murilochiquettigalina@gmail.com>
2026-03-04 22:16:48 -0300

    feat: Adding authentication with JWT and auth routes/views with user register.

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,22 +1,15 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from todo.views.register import RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('v1/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('v1/auth/register/', RegisterView.as_view(), name='auth_register'),
 ]
