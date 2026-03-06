1bf64fddf3d667933eed9b6eaadddc31199b94df
Murilo Chiquetti Galina <murilochiquettigalina@gmail.com>
2026-03-05 22:43:00 -0300

    feat: Adding password validation.

--- backend/todo/serializers/user.py
+++ backend/todo/serializers/user.py
@@ -1,5 +1,20 @@
+import re
 from rest_framework import serializers
 from django.contrib.auth.models import User
+
+
+def validate_strong_password(password):
+    if len(password) < 8:
+        raise serializers.ValidationError("The password must be at least 8 characters long.")
+    if not re.search(r'[A-Z]', password):
+        raise serializers.ValidationError("The password must contain at least one uppercase letter.")
+    if not re.search(r'[a-z]', password):
+        raise serializers.ValidationError("The password must contain at least one lowercase letter.")
+    if not re.search(r'\d', password):
+        raise serializers.ValidationError("The password must contain at least one number.")
+    if not re.search(r'[!@#$%^&*(),.?":{}|<>]', password):
+        raise serializers.ValidationError("The password must contain at least one special character.")
+    return password
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,6 +22,9 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
         
+    def validate_password(self, value):
+        return validate_strong_password(value)
+    
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
@@ -21,6 +39,9 @@ class UserUpdateSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True, 'required': False}}
 
+    def validate_password(self, value):
+        return validate_strong_password(value)
+    
     def update(self, instance, validated_data):
         if 'password' in validated_data:
             instance.set_password(validated_data.pop('password'))
