14c7bef0726c62a83c2d92de96010c7f9c5e9e5e
Murilo Chiquetti Galina <murilochiquettigalina@gmail.com>
2026-03-07 22:53:51 -0300

    feat: Making available the category_name on get task.

--- backend/todo/serializers/task.py
+++ backend/todo/serializers/task.py
@@ -6,10 +6,11 @@ class TaskSerializer(serializers.ModelSerializer):
     shared_with_usernames = serializers.SlugRelatedField(
         many=True, read_only=True, slug_field='username', source='shared_with'
     )
+    category_name = serializers.ReadOnlyField(source='category.name')
     class Meta:
         model = Task
         fields = [
             'id', 'title', 'description', 'is_completed', 'created_at', 
-            'category', 'owner', 'shared_with', 'external_quote', 'owner_username', 'shared_with_usernames'
+            'category', 'category_name', 'owner', 'shared_with', 'external_quote', 'owner_username', 'shared_with_usernames'
         ]
         read_only_fields = ['owner', 'external_quote', 'created_at',]
