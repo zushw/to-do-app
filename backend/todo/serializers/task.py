d3c45eb580213bb16eee884ded2bdd11b025b35b
Murilo Chiquetti Galina <murilochiquettigalina@gmail.com>
2026-03-07 22:30:40 -0300

    feat: Making available the list of users for sharing and those that are currently being shared.

--- backend/todo/serializers/task.py
+++ backend/todo/serializers/task.py
@@ -2,10 +2,14 @@ from rest_framework import serializers
 from ..models import Task 
 
 class TaskSerializer(serializers.ModelSerializer):
+    owner_username = serializers.ReadOnlyField(source='owner.username')
+    shared_with_usernames = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field='username', source='shared_with'
+    )
     class Meta:
         model = Task
         fields = [
             'id', 'title', 'description', 'is_completed', 'created_at', 
-            'category', 'owner', 'shared_with', 'external_quote'
+            'category', 'owner', 'shared_with', 'external_quote', 'owner_username', 'shared_with_usernames'
         ]
         read_only_fields = ['owner', 'external_quote', 'created_at',]
